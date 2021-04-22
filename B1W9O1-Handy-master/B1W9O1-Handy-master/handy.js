9b3a7c5cceb4e535553cc83d0237aca80390b99a
99050823 <99050823@mydavinci.nl>
2021-04-22 12:51:20 +0200

    Week 1 blok 2
    12:51
    22-4-2021

---
+++ B1W9O1-Handy-master/B1W9O1-Handy-master/handy.js
@@ -0,0 +1,14 @@
+var buttonDiensten = document.getElementById("diensten");
+var meerWetenButton = document.getElementById("meerWeten"); 
+
+buttonDiensten.onclick = Diensten;
+meerWetenButton.onclick = meerWeten;
+
+function Diensten () {
+    window.open("diensten.html");   
+}
+
+function meerWeten () {
+    window.open("diensten.html");
+}
+
